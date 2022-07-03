4fb1a1b54c9c44aacf7fa1c1a0aa79d62b170d23
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-07-03 15:34:50 +0800

    feat: add edit function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -25,4 +25,30 @@ router.post('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//edit
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      res.render('edit', { todo: todo.get() })
+    })
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  console.log(req.body)
+  const { name, isDone } = req.body
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
 module.exports = router
